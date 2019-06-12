df75f2b76350c2eac470a761198d27fa2cf445c2
Ruben <34570952+thepixelboy@users.noreply.github.com>
2019-06-12 13:20:13 +0200

    This is where it all begins...

---
+++ DaysOfWeek/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DaysOfWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string monday = "Monday";
+
+            string[] daysOfWeek =
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+        }
+    }
+}
