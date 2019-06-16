f6ac05e18a637c193970f6af400fd492bfa1385c
Ruben <34570952+thepixelboy@users.noreply.github.com>
2019-06-16 19:45:04 +0200

    Chapter 4. Resizing Collections with Lists.

---
+++ DaysOfWeekList/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DaysOfWeekList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> daysOfWeek = new List<string>();
+            daysOfWeek.Add("Monday");
+            daysOfWeek.Add("Tuesday");
+            daysOfWeek.Add("Wednesday");
+            daysOfWeek.Add("Thursday");
+            daysOfWeek.Add("Friday");
+            daysOfWeek.Add("Saturday");
+            daysOfWeek.Add("Sunday");
+
+            Console.WriteLine("Which day do you want to display?");
+            Console.Write("(Monday = 1, etc.) > ");
+            int iDay = int.Parse(Console.ReadLine());
+
+            string chosenDay = daysOfWeek[iDay - 1]; // Arrays are 0 index based
+            Console.WriteLine($"That days is {chosenDay}");
+        }
+    }
+}
---
+++ ReadAllCountries/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ReadAllCountries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Remember to change the path to the CSV file
+            string filePath = Path.Combine(@"C:\Users\ruben\Projects\begcshcollections\ReadAllCountries\", "countrypop-by-largest.csv");
+            CsvReader reader = new CsvReader(filePath);
+
+            List<Country> countries = reader.ReadAllCountries();
+
+            Country lilliput = new Country("Lilliput", "LIL", "Somewhere", 2_000_000);
+            int lilliputIndex = countries.FindIndex(x=>x.Population < 2_000_000);
+            countries.Insert(lilliputIndex, lilliput);
+            countries.RemoveAt(lilliputIndex);
+
+            foreach(Country country in countries)
+            {
+                Console.WriteLine($"{PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)}: {country.Name}");
+            }
+            Console.WriteLine($"{countries.Count} countries");
+        }
+    }
+}
