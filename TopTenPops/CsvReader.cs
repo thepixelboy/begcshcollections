2c00e20107b47a9c8c8e936bdfd2667cd892fdf9
Ruben <34570952+thepixelboy@users.noreply.github.com>
2019-06-16 10:46:41 +0200

    Chapter 3. Importing from a Data Source into an Array

---
+++ TopTenPops/CsvReader.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace TopTenPops
+{
+    class CsvReader
+    {
+        private string _csvFilePath;
+
+        public CsvReader(string cscFilePath)
+        {
+            this._csvFilePath = cscFilePath;
+        }
+
+        public Country[] ReadFirstNCountries(int nCountries)
+        {
+            Country[] countries = new Country[nCountries];
+
+            using (StreamReader sr = new StreamReader(_csvFilePath))
+            {
+                // read header line
+                sr.ReadLine();
+
+                for (int i = 0; i < nCountries; i++)
+                {
+                    string csvLine = sr.ReadLine();
+                    countries[i] = ReadCountryFromCsvLine(csvLine);
+                }
+            }
+
+            return countries;
+        }
+
+        public Country ReadCountryFromCsvLine(string csvLine)
+        {
+            string[] parts = csvLine.Split(new char[] {','});
+
+            string name = parts[0];
+            string code = parts[1];
+            string region = parts[2];
+            int population = int.Parse(parts[3]);
+
+            return new Country(name, code, region, population);
+        }
+    }
+}
---
+++ TopTenPops/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace TopTenPops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Remember to change the path to the CSV file
+            string filePath = Path.Combine(@"C:\Users\ruben\Projects\begcshcollections\TopTenPops\", "countrypop-by-largest.csv");
+            CsvReader reader = new CsvReader(filePath);
+
+            Country[] countries = reader.ReadFirstNCountries(10);
+
+            foreach(Country country in countries)
+            {
+                Console.WriteLine($"{PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)}: {country.Name}");
+            }
+        }
+    }
+}
