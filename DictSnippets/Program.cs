ce71a2f5be45ed3200337ae7ac05147afa8c60a0
Ruben <34570952+thepixelboy@users.noreply.github.com>
2019-06-18 11:49:37 +0200

    Chapter 5. Storing Keyed Data with Dictionaries.

---
+++ DictSnippets/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictSnippets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Country norway = new Country("Norway", "NOR", "Europe", 5_282_223);
+            Country finland = new Country("Finland", "FIN", "Europe", 5_511_303);
+
+            var countries = new Dictionary<string, Country>();
+            countries.Add(norway.Code, norway);
+            countries.Add(finland.Code, finland);
+
+            bool exists = countries.TryGetValue("MUS", out Country country);
+
+            if (exists)
+                Console.WriteLine(country.Name);
+            else
+                Console.WriteLine("There is no country with the code MUS");
+
+            //foreach (var country in countries.Values)
+            //    Console.WriteLine(country.Name);
+        }
+    }
+}
---
+++ ReadAllCountriesDict/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ReadAllCountriesDict
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Remember to change the path to the CSV file
+            string filePath = Path.Combine(@"C:\Users\ruben\Projects\begcshcollections\ReadAllCountries\", "countrypop-by-largest.csv");
+            CsvReader reader = new CsvReader(filePath);
+
+            Dictionary<string, Country> countries = reader.ReadAllCountries();
+
+            Console.WriteLine("Which country code do you want to look up? ");
+            string userInput = Console.ReadLine();
+
+            bool gotCountry = countries.TryGetValue(userInput, out Country country);
+            if (!gotCountry)
+                Console.WriteLine($"Sorry, there is no country with the code, {userInput}");
+            else
+                Console.WriteLine($"{country.Name} has population {PopulationFormatter.FormatPopulation(country.Population)}");
+        }
+    }
+}
