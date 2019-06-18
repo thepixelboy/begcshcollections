04233842369bdbb0e02671dd079c4e496d1bbf46
Ruben <34570952+thepixelboy@users.noreply.github.com>
2019-06-18 17:42:29 +0200

    Chapter 7. Selecting items using LINQ.

---
+++ ReadAllCountriesLINQ/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ReadAllCountriesLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Remember to change the path to the CSV file
+            string filePath = Path.Combine(@"C:\Users\ruben\Projects\begcshcollections\", "countrypop-by-largest.csv");
+            CsvReader reader = new CsvReader(filePath);
+
+            List<Country> countries = reader.ReadAllCountries();
+
+            var filteredCountries = countries.Where(x => !x.Name.Contains(','));//.Take(20);
+            var filteredCountries2 = from country in countries
+                                     where !country.Name.Contains(',')
+                                     select country;
+
+            foreach (Country country in filteredCountries2)
+            {
+                Console.WriteLine($"{PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)}: {country.Name}");
+            }
+        }
+    }
+}
